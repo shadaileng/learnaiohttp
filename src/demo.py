b1300207d44369e301aee50c134572773b3a907f
shadaileng <qpf0510@qq.com>
2018-06-13 16:14:03 +0800

    first commit

---
+++ src/demo.py
@@ -0,0 +1,16 @@
+#!usr/bin/python3
+#-*- coding: utf-8 -*-
+
+'''
+	*******************
+	*       Demo      *
+	*******************
+	     Powered By %s
+'''
+
+__author__ = 'Shadaileng'
+
+
+
+if __name__ == '__main__':
+	print(__doc__ % __author__)
